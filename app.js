ebcb33b556a3f9b392edc9a23904ed0a98cf68f6
Charles J. McQuain <cmcquai@wgu.edu>
2017-07-28 00:55:48 +0000

    Bootstrap Added and Basic Styling

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+   var campgrounds = [
+       {name: "Salmon Creek", image: "https://www.campsitephotos.com/photo/camp/25137/feature_Piedmont_Park-f4.jpg"},
+       {name: "Granite Hill", image: "https://www.campsitephotos.com/photo/camp/25311/feature_Prairie_Creek-f4.jpg"},
+       {name: "Mountain Hill", image: "https://www.campsitephotos.com/photo/camp/25311/feature_Prairie_Creek-f4.jpg"},
+              {name: "Salmon Creek", image: "https://www.campsitephotos.com/photo/camp/25137/feature_Piedmont_Park-f4.jpg"},
+       {name: "Granite Hill", image: "https://www.campsitephotos.com/photo/camp/25311/feature_Prairie_Creek-f4.jpg"},
+       {name: "Mountain Hill", image: "https://www.campsitephotos.com/photo/camp/25311/feature_Prairie_Creek-f4.jpg"}
+       ];
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -8,13 +20,21 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-   var campgrounds = [
-       {name: "Salmon Creek", image: "https://www.campsitephotos.com/photo/camp/25137/feature_Piedmont_Park-f4.jpg"},
-       {name: "Granite Hill", image: "https://www.campsitephotos.com/photo/camp/25311/feature_Prairie_Creek-f4.jpg"},
-       {name: "Mountain Hill", image: "https://www.campsitephotos.com/photo/camp/25311/feature_Prairie_Creek-f4.jpg"},
-       ];
+
        
        res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+   var name = req.body.name;
+   var image = req.body.image;
+   var newCampground = {name: name, image: image};
+   campgrounds.push(newCampground);
+   res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
