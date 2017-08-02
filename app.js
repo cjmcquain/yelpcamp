67425db1356c11fa484d306b8786b1556ae79aa4
Charles J. McQuain <cmcquai@wgu.edu>
2017-08-02 05:11:15 +0000

    Added Comment functionality

--- app.js
+++ app.js
@@ -1,42 +1,96 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+var seedDB = require("./seeds");
 
-   var campgrounds = [
-       {name: "Salmon Creek", image: "https://www.campsitephotos.com/photo/camp/25137/feature_Piedmont_Park-f4.jpg"},
-       {name: "Granite Hill", image: "https://www.campsitephotos.com/photo/camp/25311/feature_Prairie_Creek-f4.jpg"},
-       {name: "Mountain Hill", image: "https://www.campsitephotos.com/photo/camp/25311/feature_Prairie_Creek-f4.jpg"},
-              {name: "Salmon Creek", image: "https://www.campsitephotos.com/photo/camp/25137/feature_Piedmont_Park-f4.jpg"},
-       {name: "Granite Hill", image: "https://www.campsitephotos.com/photo/camp/25311/feature_Prairie_Creek-f4.jpg"},
-       {name: "Mountain Hill", image: "https://www.campsitephotos.com/photo/camp/25311/feature_Prairie_Creek-f4.jpg"}
-       ];
 
+mongoose.connect("mongodb://localhost/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+seedDB();
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-
-       
-       res.render("campgrounds", {campgrounds:campgrounds});
+        Campground.find({}, function(err, allCampgrounds){
+            if(err){
+                console.log(err);
+            } else {
+                res.render("campgrounds/index", {campgrounds:allCampgrounds});
+            }
+        });
 });
 
 app.post("/campgrounds", function(req, res){
    var name = req.body.name;
    var image = req.body.image;
-   var newCampground = {name: name, image: image};
-   campgrounds.push(newCampground);
-   res.redirect("/campgrounds");
+   var desc = req.body.description;
+   var newCampground = {name: name, image: image, description: desc};
+   //Create new campground and save to DB
+   Campground.create(newCampground, function(err, newlyCreated) {
+       if(err){
+           console.log(err);
+       } else {
+           res.redirect("/campgrounds");
+       }
+   });
+   
 });
 
 app.get("/campgrounds/new", function(req, res){
-    res.render("new.ejs");
+    res.render("campgrounds/new.ejs");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    //Find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+       if(err){
+           console.log(err);
+       } else {
+           res.render("campgrounds/show", {campground: foundCampground});
+       }
+    });
+});
+
+// =======================
+// COMMENTS ROUTES
+// =======================
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+    
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+           Comment.create(req.body.comment, function(err, comment){
+               if(err) {
+                   console.log(err);
+               } else {
+                   campground.comments.push(comment);
+                   campground.save();
+                   res.redirect("/campgrounds/" + campground._id);
+               }
+           });
+       }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
-   console.log("YelpCamp Server has started!") 
+   console.log("YelpCamp Server has started!");
 });
